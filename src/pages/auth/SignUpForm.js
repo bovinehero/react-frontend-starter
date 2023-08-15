5b280d1cce8c321939c98c549a30d63ce29bb985
Bovine Hero <bhero@Bovines-HeroBook-Pro.local>
2023-08-15 09:41:22 +0100

    added registration form for API testing

--- src/pages/auth/SignUpForm.js
+++ src/pages/auth/SignUpForm.js
@@ -1,20 +1,53 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useState} from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 import styles from "../../styles/SignInUpForm.module.css";
 import btnStyles from "../../styles/Button.module.css";
 import appStyles from "../../App.module.css";
 
-import { Form, Button, Image, Col, Row, Container } from "react-bootstrap";
+import { Form, Button, Image, Col, Row, Container, Alert } from "react-bootstrap";
+import axios from "axios";
 
 const SignUpForm = () => {
+    const [signUpData, setSignUpData] = useState({
+        username: '',
+        password1: '',
+        password2: '',
+    });
+    const { username, password1, password2 } = signUpData;
+
+    const history = useNavigate();
+
+    const [errors, setErrors] = useState({});
+
+    const handleChange = (event) => {
+        setSignUpData({
+            // load the initial data set
+            ...signUpData,
+            // creates KV pair for the event, tracks against the name of the and attaches the new value that has changed
+            // this allows reuse - so we add to each form component
+            [event.target.name]: event.target.value,
+        });
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            await axios.post('/dj-rest-auth/registration/', signUpData)
+            history.push("/signin")
+        } catch(err){
+            // ? siginifies conditional chaining, so if not available don't throw an error
+            setErrors(err.respomse?.data)
+        }
+    }
+
   return (
     <Row className={styles.Row}>
       <Col className="my-auto py-2 p-md-2" md={6}>
         <Container className={`${appStyles.Content} p-4 `}>
           <h1 className={styles.Header}>sign up</h1>
 
-          <Form>
+          <Form onSubmit={handleSubmit}>
             <Form.Group className="mb-3" controlId="username">
                 <Form.Label className="d-none">Username</Form.Label>
                 <Form.Control 
@@ -22,8 +55,15 @@ const SignUpForm = () => {
                     type="text" 
                     placeholder="Username"
                     name="username"
+                    value={username}
+                    onChange={handleChange}
                 />
             </Form.Group>
+            {/* {errors.username?.map((message, idx) => (
+              <Alert variant="warning" key={idx}>
+                {message}
+              </Alert>
+            ))} */}
 
             <Form.Group className="mb-3" controlId="password1">
                 <Form.Label className="d-none">Password</Form.Label>
@@ -31,7 +71,9 @@ const SignUpForm = () => {
                     className={styles.Input}
                     type="password" 
                     placeholder="Password" 
-                    name="password1" 
+                    name="password1"
+                    value={password1}
+                    onChange={handleChange}
                 />
             </Form.Group>
 
@@ -41,9 +83,12 @@ const SignUpForm = () => {
                     className={styles.Input}
                     type="password" 
                     placeholder="Confirm Password" 
-                    name="password2" 
+                    name="password2"
+                    value={password2}
+                    onChange={handleChange}
                 />
             </Form.Group>
+            
 
             <Button type="submit" className={`${btnStyles.Button} ${btnStyles.Wide} ${btnStyles.Bright}`}>
                 Sign Up
