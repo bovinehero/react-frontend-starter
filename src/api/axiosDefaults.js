4d69decdb7fc9a7e92e46f3a0d824910cb1c7330
Bovine Hero <bhero@Bovines-HeroBook-Pro.local>
2023-08-17 20:10:06 +0100

    adding switching to prod config for testing

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // chnage to this for prod
-// axios.defaults.baseURL = 'https://bhero-django-aws-tester-d28ab74c5855.herokuapp.com/';
-axios.defaults.baseURL = 'http://127.0.0.1:8000/';
+axios.defaults.baseURL = 'https://bhero-django-aws-tester-d28ab74c5855.herokuapp.com/';
+// axios.defaults.baseURL = 'http://127.0.0.1:8000/';
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data';
 axios.defaults.withCredentials = true;
